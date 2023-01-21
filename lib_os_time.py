2ad637539815e282fa2621944bf7b1c7f4631ac0
To ja <student-wroclaw@EDU-WSB.local>
2023-01-21 12:00:44 +0100

    commit2

--- lib_os_time.py
+++ lib_os_time.py
@@ -1,8 +1,11 @@
 import os
+import time
 
 print(os.getcwd())
 os.chdir('C:\\Users\\vdi-student\\Desktop')
 print(os.getcwd())
 os.mkdir('new_folder')
+time.sleep(2)
 os.rename('new_folder', 'new_folder_2')
+time.sleep(2)
 os.rmdir('new_folder_2')
